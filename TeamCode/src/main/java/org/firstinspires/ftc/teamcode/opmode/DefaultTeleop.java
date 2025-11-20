ab63f250e9cbc078619957ff3157fb864bcd2cb4
13600k <123127287+13600k@users.noreply.github.com>
2025-11-19 23:16:37 -0500

    Update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DefaultTeleop.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+import java.util.function.Supplier;
+
+@TeleOp
+public class DefaultTeleop extends OpMode {
+    private Follower follower;
+    public static Pose startingPose; //See ExampleAuto to understand how to use this
+    private boolean automatedDrive;
+    private Supplier<PathChain> pathChain;
+
+    @Override
+    public void init() {
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startingPose == null ? new Pose() : startingPose);
+        follower.update();
+    }
+
+    @Override
+    public void loop() {
+        follower.update();
+        //Make the last parameter false for field-centric
+        //In case the drivers want to use a "slowMode" you can scale the vectors
+        //This is the normal version to use in the TeleOp
+        follower.setTeleOpDrive(
+            -gamepad1.left_stick_y,
+            -gamepad1.left_stick_x,
+            -gamepad1.right_stick_x,
+            true // Robot Centric
+        );
+    }
+}
